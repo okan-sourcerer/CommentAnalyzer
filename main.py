6007b159fee6be12a1a5214c3a56e52d59c627be
okan-sourcerer <58114457+okan-sourcerer@users.noreply.github.com>
2023-05-19 22:44:38 +0300

    Basic performance measuring and modified console output to be a progress bar

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import time
+
 import webScraper
 
 if __name__ == "__main__":
+    tic = time.perf_counter()
     comments = webScraper.scrape_for_comments("https://www.imdb.com/title/tt0460649/reviews/?ref_=ttrt_ql_2")
 
+    toc = time.perf_counter()
+    print(f"Scraped data in {toc - tic:0.4f} seconds")
     print("printing comments ", len(comments))
     for comment in comments:
         print(comment)
         print('*' * 30)
+
+    tac = time.perf_counter()
+    print(f"Printed data in {tac - toc:0.4f} seconds")
--- webScraper.py
+++ webScraper.py
@@ -1,3 +1,4 @@
+import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,16 +12,17 @@ def scrape_for_comments(imdb_url):
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
     driver.get(imdb_url)
-
     comments_dict = {}
 
     try:
         did_load_all = False
         while True:
-            print("Reading comments!")
+            # print("Reading comments!")
             # get the current page source and create a Beautiful Soup object
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
+
+            total_reviews = int(soup.select('div.header span:not(:empty,:has(div))')[0].text.split(' ')[0])
             comments_list = soup.select('div.lister-list')
             comments = []
             for single_comment in comments_list[0].contents:
@@ -36,18 +38,21 @@ def scrape_for_comments(imdb_url):
                 if review_id not in comments_dict:
                     comments_dict[review_id] = comment_text
 
+            each_step = total_reviews / 50
+            progress = int(len(comments_dict) / each_step)
+            sys.stdout.write("\rLoading comments: " + str(len(comments_dict)) + f'/{total_reviews}' +
+                             " [" + ("=" * progress) + (">" * ((50 - progress) > 0) + (" " * (49 - progress)) + "]"))
+
             if did_load_all:
                 break
 
             try:
-                print("loading comments.. current comments length is ", len(comments_dict))
+                # print("loading comments.. current comments length is ", len(comments_dict))
                 load_more_button = WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.CLASS_NAME, 'load-more-data'))
                 )
                 load_more_button.click()
-            except Exception as e:
-                print("exception has happened")
-                print(e)
+            except Exception as _:
                 did_load_all = True
                 continue
     finally:
