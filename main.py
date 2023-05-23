4e17365972852242ffc18bf5458e42ce2524ef98
okan-sourcerer <58114457+okan-sourcerer@users.noreply.github.com>
2023-05-23 17:47:00 +0300

    Refactored the whole project. Switched to requests + BeautifulSoup. Increased system speed at minimum 4 times.

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+import cProfile
+import pstats
 import time
 
 import webScraper
 
 if __name__ == "__main__":
     tic = time.perf_counter()
-    comments = webScraper.scrape_for_comments("https://www.imdb.com/title/tt0460649/reviews/?ref_=ttrt_ql_2")
+    imdb_url = "https://www.imdb.com/title/tt0460649/reviews"
 
-    toc = time.perf_counter()
-    print(f"Scraped data in {toc - tic:0.4f} seconds")
-    print("printing comments ", len(comments))
+    profiler = cProfile.Profile()
+    profiler.enable()
+    comments = webScraper.scrape_for_comments(imdb_url)
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats(pstats.SortKey.CUMULATIVE)
+    stats.print_stats(30)
+    tac = time.perf_counter()
+
     for comment in comments:
         print(comment)
-        print('*' * 30)
-
-    tac = time.perf_counter()
-    print(f"Printed data in {tac - toc:0.4f} seconds")
+        print(30 * "*")
+    toc = time.perf_counter()
+    print(f"Printed data in {toc - tac:0.4f} seconds")
--- webScraper.py
+++ webScraper.py
@@ -1,61 +1,108 @@
 import sys
+import time
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+time_find_specific = 0
+time_find_all_next = 0
+time_extract_comment_text = 0
+time_find_review_count = 0
+time_get_source = 0
+time_init_soups = 0
+comments_dict = {}
 
 
-def scrape_for_comments(imdb_url):
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
-    driver.get(imdb_url)
-    comments_dict = {}
+def parse_comments(comment_soup, main_url):
+    global time_find_specific
+    global time_find_all_next
+    global time_extract_comment_text
 
-    try:
-        did_load_all = False
-        while True:
-            # print("Reading comments!")
-            # get the current page source and create a Beautiful Soup object
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser')
+    comments = []
+    start_find_specific = time.perf_counter()
+    specific_element = comment_soup.select_one('div.lister-list')
+    time_find_specific += time.perf_counter() - start_find_specific
 
-            total_reviews = int(soup.select('div.header span:not(:empty,:has(div))')[0].text.split(' ')[0])
-            comments_list = soup.select('div.lister-list')
-            comments = []
-            for single_comment in comments_list[0].contents:
-                if single_comment != '\n':
-                    comments.append(single_comment)
+    start_find_all_next = time.perf_counter()
+    following_elements = specific_element.find_all_next(lambda tag: tag.has_attr('data-review-id'))
+    time_find_all_next += time.perf_counter() - start_find_all_next
 
-            for current_index in range(len(comments)):
-                comment = comments[current_index]
-                review_id = comment['data-review-id']
+    comments.extend(following_elements)
 
-                comment_text = comment.findAll('div', class_='text')[0].text
+    for comment in comments:
+        review_id = comment['data-review-id']
 
-                if review_id not in comments_dict:
-                    comments_dict[review_id] = comment_text
+        start_extract_comment = time.perf_counter()
+        comment_text = comment.find('div', class_='text').text
+        comments_dict[review_id] = comment_text
+        time_extract_comment_text += time.perf_counter() - start_extract_comment
+
+    pagination_key = comment_soup.find('div', class_="load-more-data")
+    if pagination_key is not None:
+        next_url = f"{main_url}/_ajax?ref_=undefined&paginationKey=" \
+              f"{pagination_key['data-key']}"
+    else:
+        next_url = ""
+
+    return next_url
+
+
+def scrape_for_comments(url):
+    global time_find_specific
+    global time_find_all_next
+    global time_extract_comment_text
+    global time_find_review_count
+    global time_get_source
+    global time_init_soups
+
+    tic = time.perf_counter()
+    main_url = url
+
+    request_session = requests.Session()
+
+    start_page_source = time.perf_counter()
+    response = request_session.get(url)
+    time_get_source += time.perf_counter() - start_page_source
+    start_init_soup = time.perf_counter()
+    soup = BeautifulSoup(response.text, 'lxml')
+    time_init_soups += time.perf_counter() - start_init_soup
+
+    start_review_count = time.perf_counter()
+    total_reviews = int(soup.select('div.header span:not(:empty,:has(div))')[0].text.split(' ')[0])
+    time_find_review_count += time.perf_counter() - start_review_count
+
+    url = parse_comments(soup, main_url)
+
+    while url != "":
+        start_page_source = time.perf_counter()
+        response = request_session.get(url)
+        time_get_source += time.perf_counter() - start_page_source
+        # Check the response status code
+        if response.status_code == requests.codes.ok:
+            # Successful request
+            start_init_soup = time.perf_counter()
+            soup = BeautifulSoup(response.text, 'lxml')
+            time_init_soups += time.perf_counter() - start_init_soup
+
+            url = parse_comments(soup, main_url)
 
             each_step = total_reviews / 50
             progress = int(len(comments_dict) / each_step)
             sys.stdout.write("\rLoading comments: " + str(len(comments_dict)) + f'/{total_reviews}' +
                              " [" + ("=" * progress) + (">" * ((50 - progress) > 0) + (" " * (49 - progress)) + "]"))
+        else:
+            # Request failed
+            print("Request failed with status code:", response.status_code)
+            url = ""
 
-            if did_load_all:
-                break
+    print(f"\nAll comments {len(comments_dict)}")
+    print(f"\nfind specific {time_find_specific}"
+          f"\n, find all next {time_find_all_next}"
+          f"\n, extract text {time_extract_comment_text}"
+          f"\n, find review count {time_find_review_count}"
+          f"\n, init soups {time_init_soups}"
+          f"\n, load page source {time_get_source}")
 
-            try:
-                # print("loading comments.. current comments length is ", len(comments_dict))
-                load_more_button = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'load-more-data'))
-                )
-                load_more_button.click()
-            except Exception as _:
-                did_load_all = True
-                continue
-    finally:
-        driver.quit()
+    toc = time.perf_counter()
 
+    print(f"Total time: {toc - tic}")
     return comments_dict.values()
