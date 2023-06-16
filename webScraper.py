e541648ca39b39e42f46ef2c5a628959e75f01a0
okan-sourcerer <58114457+okan-sourcerer@users.noreply.github.com>
2023-06-16 21:32:10 +0300

    Added comments to codes. Added machine learning models (SVM, Logistic regression, Naive Bayes, Decision Tree). Implemented web scraping functions to read from imdb.

--- webScraper.py
+++ webScraper.py
@@ -3,6 +3,7 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
+# timer variables for debug purposes
 time_find_specific = 0
 time_find_all_next = 0
 time_extract_comment_text = 0
@@ -19,15 +20,16 @@ def parse_comments(comment_soup, main_url):
 
     comments = []
     start_find_specific = time.perf_counter()
-    specific_element = comment_soup.select_one('div.lister-list')
+    specific_element = comment_soup.select_one('div.lister-list')  # read all comments
     time_find_specific += time.perf_counter() - start_find_specific
 
     start_find_all_next = time.perf_counter()
-    following_elements = specific_element.find_all_next(lambda tag: tag.has_attr('data-review-id'))
+    following_elements = specific_element.find_all_next(lambda tag: tag.has_attr('data-review-id'))  # get review ids
     time_find_all_next += time.perf_counter() - start_find_all_next
 
     comments.extend(following_elements)
 
+    # set ids to comments and add to dictionary
     for comment in comments:
         review_id = comment['data-review-id']
 
@@ -36,12 +38,12 @@ def parse_comments(comment_soup, main_url):
         comments_dict[review_id] = comment_text
         time_extract_comment_text += time.perf_counter() - start_extract_comment
 
-    pagination_key = comment_soup.find('div', class_="load-more-data")
+    pagination_key = comment_soup.find('div', class_="load-more-data")  # get next comment's url
     if pagination_key is not None:
         next_url = f"{main_url}/_ajax?ref_=undefined&paginationKey=" \
               f"{pagination_key['data-key']}"
     else:
-        next_url = ""
+        next_url = ""  # end of the comments
 
     return next_url
 
@@ -57,15 +59,16 @@ def scrape_for_comments(url):
     tic = time.perf_counter()
     main_url = url
 
-    request_session = requests.Session()
+    request_session = requests.Session()  # create sessions instead of request instance for each connection
 
     start_page_source = time.perf_counter()
-    response = request_session.get(url)
+    response = request_session.get(url)  # open url
     time_get_source += time.perf_counter() - start_page_source
     start_init_soup = time.perf_counter()
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, 'lxml')  # parse page source with lxml.
     time_init_soups += time.perf_counter() - start_init_soup
 
+    # get total review count
     start_review_count = time.perf_counter()
     total_reviews = int(soup.select('div.header span:not(:empty,:has(div))')[0].text.split(' ')[0])
     time_find_review_count += time.perf_counter() - start_review_count
@@ -74,17 +77,18 @@ def scrape_for_comments(url):
 
     while url != "":
         start_page_source = time.perf_counter()
-        response = request_session.get(url)
+        response = request_session.get(url)  # open session
         time_get_source += time.perf_counter() - start_page_source
         # Check the response status code
         if response.status_code == requests.codes.ok:
             # Successful request
             start_init_soup = time.perf_counter()
-            soup = BeautifulSoup(response.text, 'lxml')
+            soup = BeautifulSoup(response.text, 'lxml')  # parse page source
             time_init_soups += time.perf_counter() - start_init_soup
 
-            url = parse_comments(soup, main_url)
+            url = parse_comments(soup, main_url)  # parse comments and get next comment's url
 
+            # progress bar
             each_step = total_reviews / 50
             progress = int(len(comments_dict) / each_step)
             sys.stdout.write("\rLoading comments: " + str(len(comments_dict)) + f'/{total_reviews}' +
